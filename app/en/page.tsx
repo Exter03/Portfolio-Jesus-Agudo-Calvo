abb3342dedce43dda400de33308d74c22da5fc46
Exter03 <jes.agucalvo@gmail.com>
2026-01-12 01:17:51 +0100

    Tout prêt pour déploiement

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -28,7 +28,7 @@ export default function EnglishHome() {
           background: { color: { value: "transparent" } },
           fpsLimit: 60,
           particles: {
-            number: { value: 90, density: { enable: true, area: 800 } },
+            number: { value: 90, density: { enable: true, value_area: 800 } },
             color: { value: ["#00f0ff", "#0ff", "#0fffb0"] },
             shape: { type: "circle" },
             opacity: { value: 0.5 },
